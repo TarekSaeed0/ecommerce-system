5b4c26937fb441dc33cfddb83ee8649340033de0
TarekSaeed0 <naminode2020@gmail.com>
2026-04-06 07:39:10 +0200

    feat: add customer class with unit tests

---
+++ src/main/java/com/github/tareksaeed0/Customer.java
@@ -0,0 +1,57 @@
+package com.github.tareksaeed0;
+
+public class Customer {
+	private String name;
+	private double balance;
+
+	public Customer(String name, double InitialBalance) {
+		setName(name);
+		setBalance(InitialBalance);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		if (name == null || name.isBlank()) {
+			throw new IllegalArgumentException("Name can't be null or empty.");
+		}
+
+		this.name = name;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	private void setBalance(double balance) {
+		if (balance < 0) {
+			throw new IllegalArgumentException("Balance can't be negative.");
+		}
+
+		this.balance = balance;
+	}
+
+	public void deposit(double amount) {
+		if (amount < 0) {
+			throw new IllegalArgumentException("Deposited amount can't be negative.");
+		}
+
+		this.balance += amount;
+	}
+
+
+	public void withdraw(double amount) {
+		if (amount < 0) {
+			throw new IllegalArgumentException("Withdrawn amount can't be negative.");
+		}
+
+		if (amount > balance) {
+			throw new IllegalArgumentException(
+					"Withdrawn amount can't be greater than the balance.");
+		}
+
+		this.balance -= amount;
+	}
+}
