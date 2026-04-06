7a020121fce2d1061d1725758055aa900a5e4afa
TarekSaeed0 <naminode2020@gmail.com>
2026-04-06 07:50:50 +0200

    feat: add cart class and its unit tests

--- src/main/java/com/github/tareksaeed0/App.java
+++ src/main/java/com/github/tareksaeed0/App.java
@@ -1,10 +1,27 @@
 package com.github.tareksaeed0;
 
-/**
- * Hello world!
- */
+import java.time.LocalDateTime;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Product laptop = new ShippableProduct("Laptop", 999.99, 10, 2.5);
+        Product cheese = new ExpirableShippableProduct("Cheese", 5.99, 20,
+                LocalDateTime.now().plusDays(7), 0.5);
+        Product scratchCard = new Product("Scratch Card", 1.00, 100);
+
+        Customer customer = new Customer("Tarek", 1500);
+
+        System.out.println("Customer: " + customer.getName());
+        System.out.println("Balance: $" + customer.getBalance());
+
+        Cart cart = new Cart();
+        cart.add(laptop, 1);
+        cart.add(cheese, 3);
+        cart.add(scratchCard, 2);
+
+        for (Cart.Item item : cart) {
+            System.out.println(
+                    item.getQuantity() + "x " + item.getProduct().getName());
+        }
     }
 }
